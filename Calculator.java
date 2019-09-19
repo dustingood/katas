98452f5e6616df83d7b41979c84ddc287a9bb54a
Dustin J Good <55038219+dustingood@users.noreply.github.com>
2019-09-18 22:18:21 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,14 @@ package com.example.katas;
 
 import java.util.StringTokenizer;
 
+/**
+ * 1. A null or empty string should return 0
+ * 2. A single number should return itself
+ * 3. Two comma separated numbers should return the sum of the numbers
+ * 4. Three or more comma separated numbers should return the sum of the numbers
+ * 5. A Negative number should throw an IllegalArgumentException
+ * 6. Numbers Greater than or Equal to 1000 should be ignored
+ */
 public class Calculator {
 
     public int add(String s){
